b3569650b4673632f1c64cf976aed4ad3076687b
FabloAlencar <57524853+FabloAlencar@users.noreply.github.com>
2019-12-09 21:07:07 +0100

    C02: 33. Exercise 1: Add membership type to list of customers

---
+++ Vidly/Migrations/201912091824548_AddSubcribedToNewsToCostumer.cs
@@ -0,0 +1,18 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddSubcribedToNewsToCostumer : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "IsSubcraibedToNewsletter", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "IsSubcraibedToNewsletter");
+        }
+    }
+}
